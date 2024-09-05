79c01ce968d6f18b82ae86b8cac1f2883fb7fd45
Mayron <mayron_doc@outlook.com>
2024-09-05 18:08:51 -0300

    Adicionar arquivos de projeto.

---
+++ StringVerify/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program {
+    static void Main(string[] args) {
+        // Entrada do usuário
+        Console.Write("Digite algo: ");
+        string value = Console.ReadLine().ToLower();
+
+        // Obtendo número de ocorrência do caractere e fazendo a validação caso não tenha nenhuma ocorrência
+        int count = value.Count(i => i == 'a');
+        if (count > 0) {
+            Console.WriteLine($"Há {count} ocorrências do caractere 'a'");
+        }
+        else { Console.WriteLine("Não há nenhuma ocorrência do caractere 'a'"); }
+    }
+}
